f1774694e2b6373e5651dd9f49e09633d217f410
Andrew Younger <73725558+KrakenVVagon@users.noreply.github.com>
2023-05-14 14:48:59 -0400

    created LanguageDetector CNN

--- src/languageDetectinator/models.py
+++ src/languageDetectinator/models.py
@@ -10,7 +10,28 @@ from torch import optim
 
 class LanguageDetector(nn.Module):
 
-    def __init__(self) -> None:
+    def __init__(self, inputSize: int, hiddenSize: int, outputSize: int) -> None:
         super(LanguageDetector,self).__init__()
-
+        self.inputSize = inputSize
+        self.hiddenSize = hiddenSize
+        self.outputSize = outputSize
+        self.conv1 = nn.Conv1d(self.inputSize,self.hiddenSize,kernel_size=3,padding=1)
+        self.pool1 = nn.MaxPool1d(kernel_size=2)
+        self.conv2 = nn.Conv1d(self.hiddenSize,self.hiddenSize,kernel_size=3,padding=1)
+        self.pool2 = nn.MaxPool1d(kernel_size=2)
+        self.fc1 = nn.Linear(self.hiddenSize*32,512)
+        self.fc2 = nn.Linear(512,self.outputSize)
         return None
+    
+    def forward(self, x):
+        x = self.conv1(x)
+        x = nn.functional.relu(x)
+        x = self.pool1(x)
+        x = self.conv2(x)
+        x = nn.functional.relu(x)
+        x = self.pool2(x)
+        x = x.view(-1, self.hiddenSize * 32)
+        x = self.fc1(x)
+        x = nn.functional.relu(x)
+        x = self.fc2(x)
+        return x
