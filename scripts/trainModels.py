aa733dae07e258efcb32fb6937de288b4d8eb4b7
Andrew Younger <73725558+KrakenVVagon@users.noreply.github.com>
2023-06-06 09:12:27 -0400

    moved into train file

--- scripts/trainModels.py
+++ scripts/trainModels.py
@@ -1,106 +1,141 @@
 """Train and measure performance of different models.
 
 """
-
-from languageDetectinator.models import LanguageDetector_RNN, LanguageDetector_FFNN, ModelTrainer
+from languageDetectinator.models import LanguageDetector_RNN, RNN
+import glob
+import os
 import numpy as np
 import torch
 from torch import nn
 from torch import optim
 from sklearn.model_selection import train_test_split
 from torch.utils.data import TensorDataset, DataLoader
+from typing import Iterable, Tuple
+import random
 
-def loadData(filePath, langNum):
-    """Load some vectorized data to be fed into the model
+def findFiles(path): return glob.glob(path)
+
+def readNames(fileName: str) -> list:
+    lines = open(fileName, encoding="utf-8").read().split(" ")
+    return lines
+
+def letterToIndex(letter: str) -> int:
+    return ord(letter)-97
+
+def lineToTensor(line: str) -> torch.TensorType:
+    tensor = torch.zeros(len(line), 1, 26)
+    for ind, letter in enumerate(line):
+        tensor[ind][0][letterToIndex(letter)] = 1
+    return tensor
+
+def categoryFromOutput(output: torch.TensorType, categories: Iterable[str]) -> Tuple[str, int]:
+    _, top_i = output.topk(1)
+    category_i = top_i[0].item()
+    return categories[category_i], category_i
+
+def randomChoice(l):
+    return l[random.randint(0, len(l) - 1)]
+
+def randomTrainingExample(categories: Iterable[str], categoryText: dict) -> Tuple[str, str, torch.TensorType, torch.TensorType]:
+    category = randomChoice(categories)
+    line = randomChoice(categoryText[category])
+    category_tensor = torch.tensor([categories.index(category)], dtype=torch.long)
+    line_tensor = lineToTensor(line)
+    return category, line, category_tensor, line_tensor
+
+def train(model, category_tensor, line_tensor, optimizer, criterion):
+    hidden = model.initHidden()
+
+    model.zero_grad()
+
+    for i in range(line_tensor.size()[0]):
+        output, hidden = model(line_tensor[i], hidden)
+
+    loss = criterion(output, category_tensor)
+    loss.backward()
+    optimizer.step()
+
+    return output, loss.item()
+
+def evaluate(model, line_tensor):
+    hidden = model.initHidden()
+
+    for i in range(line_tensor.size()[0]):
+        output, hidden = model(line_tensor[i], hidden)
+
+    return output
+
+categories = []
+categoryText = {}
+
+for fileName in findFiles("data/processed/*.txt"):
+    category = os.path.splitext(os.path.basename(fileName))[0]
+    categories.append(category)
+    lines = readNames(fileName)
+    categoryText[category] = lines
+
+nCategories = len(categories)
+n_hidden = 128
+rnn = RNN(26, n_hidden, nCategories)
+criterion = nn.NLLLoss()
+optimizer = optim.SGD(rnn.parameters(), lr=0.005)
+
+n_iters = 100000
+print_every = 5000
+
+losses = []
+currentLoss = 0
+
+for iter in range(1, n_iters + 1):
+    category, line, category_tensor, line_tensor = randomTrainingExample(categories, categoryText)
+    output, loss = train(rnn, category_tensor, line_tensor, optimizer, criterion)
+    currentLoss += loss
+
+    if iter % print_every == 0:
+        guess, guess_i = categoryFromOutput(output, categories)
+        correct = '✓' if guess == category else f'✗ {category}'
+        print(f"{iter}, {iter/n_iters*100}%, {loss}, {line}, {guess}, {correct}")
+
+    if iter % 1000 == 0:
+        losses.append(currentLoss/1000)
+        currentLoss = 0
+
+n_confusion = 10000
+correct = 0
+
+for i in range(n_confusion):
+    category, line, category_tensor, line_tensor = randomTrainingExample(categories, categoryText)
+    output = evaluate(rnn, line_tensor)
+    guess, guess_i = categoryFromOutput(output, categories)
+    if guess == category:
+        correct += 1
+
+print(f"Overall accuracy: {correct/n_confusion*100:.4f}%")
+
+# def validate(validationData: tuple, batch_size: int, criterion: nn.Module, model: nn.Module, hidden) -> tuple:
+#     """Validation loop in order to get model accuracy while training. Can also be used for testing.
     
-    """
-    data = np.load(filePath)
-    inputs = data[:,:-langNum]
-    labels = data[:,-langNum:]
-    return inputs, labels
+#     """
+#     validationLoader = DataLoader(_loadData(validationData[0],validationData[1]), batch_size=batch_size, drop_last=True)
 
-def _loadData(inputs: np.array, labels: np.array) -> TensorDataset:
-    """Transform the data from numpy arrays into PyTorch tensors.
-    
-    """
-    inputTensor = torch.from_numpy(inputs).float()
-    labelTensor = torch.from_numpy(labels).long()
+#     model.eval()
+#     with torch.no_grad():
+#         valLoss = 0
+#         totalSamples = 0
+#         correctSamples = 0
 
-    return TensorDataset(inputTensor, labelTensor)
+#         for i, (inputTensor, labelTensor) in enumerate(validationLoader):
+#             inputTensor = torch.permute(inputTensor, (1, 0, 2))
+#             inputTensor = inputTensor.to(device)
+#             labelTensor = labelTensor.to(device)
 
-def validate(validationData: tuple, batch_size: int, criterion: nn.Module, model: nn.Module, hidden) -> tuple:
-    """Validation loop in order to get model accuracy while training. Can also be used for testing.
-    
-    """
-    validationLoader = DataLoader(_loadData(validationData[0],validationData[1]), batch_size=batch_size, drop_last=True)
+#             validationOutputs, hidden = model(inputTensor, hidden)
+#             valLoss += criterion(validationOutputs[-1], labelTensor).item()
+#             _, predicted = torch.max(validationOutputs[-1].data, 1)
+#             totalSamples += labelTensor.size(0)
+#             correctSamples += (predicted == labelTensor).sum().item()
 
-    model.eval()
-    with torch.no_grad():
-        valLoss = 0
-        totalSamples = 0
-        correctSamples = 0
+#         valLoss /= len(validationLoader)
+#         valAccuracy = correctSamples / totalSamples
 
-        for i, (inputTensor, labelTensor) in enumerate(validationLoader):
-            inputTensor = torch.permute(inputTensor, (1, 0, 2))
-            inputTensor = inputTensor.to(device)
-            labelTensor = labelTensor.to(device)
-
-            validationOutputs, hidden = model(inputTensor, hidden)
-            valLoss += criterion(validationOutputs[-1], labelTensor).item()
-            _, predicted = torch.max(validationOutputs[-1].data, 1)
-            totalSamples += labelTensor.size(0)
-            correctSamples += (predicted == labelTensor).sum().item()
-
-        valLoss /= len(validationLoader)
-        valAccuracy = correctSamples / totalSamples
-
-    return (valLoss, valAccuracy)
-
-def train(model: nn.Module, epochs: int, inputs: np.array, labels: np.array, optimzier: optim.Optimizer, criterion: nn.Module, batch_size: int=32, validation_data: tuple=None) -> list:
-    trainLoader = DataLoader(_loadData(inputs,labels),batch_size=batch_size, drop_last=True)
-
-    history = []
-    for e in range(epochs):
-        epochLoss = 0
-        for i, (inputTensor, labelTensor) in enumerate(trainLoader):
-            inputTensor = torch.permute(inputTensor, (1, 0, 2))
-            inputTensor = inputTensor.to(device)
-            labelTensor = labelTensor.to(device)
-
-            h1 = torch.zeros(12, inputTensor.size()[1], 128)
-            h2 = torch.zeros(12, inputTensor.size()[1], 256)
-            h3 = torch.zeros(12, inputTensor.size()[1], 512)
-            h4 = torch.zeros(12, inputTensor.size()[1], 1024)
-
-            hidden = [h1, h2, h3, h4]
-
-            optimzier.zero_grad()
-            outputTensor, hidden =  model(inputTensor, hidden)
-            loss = criterion(outputTensor[-1],labelTensor)
-            loss.backward()
-            optimzier.step()
-
-            epochLoss += loss.item()
-
-        valLoss, valAccuracy = 0, 0
-        if validation_data is not None:
-            valLoss, valAccuracy = validate(validation_data, batch_size, criterion, model, hidden)
-            
-        epochLoss /= len(trainLoader)
-        history.append((epochLoss, valLoss, valAccuracy))
-        print(f"Epoch: {e+1}; loss: {epochLoss:.4f}; valLoss: {valLoss:.4f}; valAcc: {valAccuracy:.4f}")
-
-    return history
-
-langNum = 5
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-rnn = LanguageDetector_RNN(26, 5, [128, 256])
-optimizer = optim.Adam(rnn.parameters(), lr=0.00001)
-inputs = np.load("./data/processed/tokenVectors.npy")
-labels = np.load("./data/processed/tokenLabels.npy")
-
-x_train, x_pre, y_train, y_pre = train_test_split(inputs,labels,test_size=0.2)
-x_val, x_test, y_val, y_test = train_test_split(x_pre, y_pre, test_size=0.5)
-
-history = train(rnn, 100, x_train, y_train, optimizer, nn.NLLLoss(), batch_size=32, validation_data=(x_val,y_val))
+#     return (valLoss, valAccuracy)
--- src/languageDetectinator/models.py
+++ src/languageDetectinator/models.py
@@ -7,55 +7,25 @@ from torch import nn
 from torch import optim
 from torch.utils.data import TensorDataset, DataLoader
 
-class LanguageDetector_CNN(nn.Module):
-    """CNN version of a language detector network
-    
-    """
-    def __init__(self, inputSize: int, hiddenSize: int, outputSize: int) -> None:
-        super(LanguageDetector_CNN,self).__init__()
-        self.inputSize = inputSize
-        self.hiddenSize = hiddenSize
-        self.outputSize = outputSize
-        self.conv1 = nn.Conv1d(self.inputSize,self.hiddenSize,kernel_size=3,padding=1)
-        self.pool1 = nn.MaxPool1d(kernel_size=1)
-        self.conv2 = nn.Conv1d(self.hiddenSize,self.hiddenSize,kernel_size=3,padding=1)
-        self.pool2 = nn.MaxPool1d(kernel_size=1)
-        self.fc1 = nn.Linear(self.hiddenSize*32,512)
-        self.fc2 = nn.Linear(512,self.outputSize)
-        return None
-    
-    def forward(self, x):
-        x = self.conv1(x)
-        x = nn.functional.relu(x)
-        x = self.pool1(x)
-        x = self.conv2(x)
-        x = nn.functional.relu(x)
-        x = self.pool2(x)
-        x = x.view(-1, self.hiddenSize * 32)
-        x = self.fc1(x)
-        x = nn.functional.relu(x)
-        x = self.fc2(x)
-        return x
-    
-class LanguageDetector_FFNN(nn.Module):
-    """More traditional FFNN language detector network
-    
-    """
-    def __init__(self, inputSize: int, outputSize: int):
-        super(LanguageDetector_FFNN,self).__init__()
-        self.inputSize = inputSize
-        self.outputSize = outputSize
-        self.fc1 = nn.Linear(self.inputSize, 32)
-        self.fc5 = nn.Linear(32, self.outputSize)
-        self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(0.5)
-        self.softmax = nn.Softmax(dim=1)
-        return None
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RNN, self).__init__()
 
-    def forward(self,x):
-        x = self.relu(self.fc1(x))
-        x = self.softmax(self.fc5(x))
-        return x
+        self.hidden_size = hidden_size
+
+        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
+        self.i2o = nn.Linear(input_size + hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input, hidden):
+        combined = torch.cat((input, hidden), 1)
+        hidden = self.i2h(combined)
+        output = self.i2o(combined)
+        output = self.softmax(output)
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, self.hidden_size)
     
 class LanguageDetector_RNN(nn.Module):
     """Character level RNN like the tutorial uses
@@ -94,76 +64,3 @@ class LanguageDetector_RNN(nn.Module):
         for size in self.hiddenSizes:
             hidden_states.append(torch.zeros(sequence_length, batch_size, size))
         return hidden_states
-
-class ModelTrainer():
-    """General framework to train PyTorch models
-    
-    """
-    def __init__(self, model: nn.Module, device: torch.DeviceObjType):
-        self.model = model
-        self.device = device
-        return None
-    
-    def _loadData(self, inputs: np.array, labels: np.array) -> TensorDataset:
-        """Transform the data from numpy arrays into PyTorch tensors.
-        
-        """
-        inputTensor = torch.from_numpy(inputs).float()
-        labelTensor = torch.from_numpy(labels)
-        return TensorDataset(inputTensor, labelTensor)
-    
-    def validate(self, validationData: tuple, batch_size: int, criterion: nn.Module) -> tuple:
-        """Validation loop in order to get model accuracy while training. Can also be used for testing.
-        
-        """
-        validationLoader = DataLoader(self._loadData(validationData[0],validationData[1]), batch_size=batch_size)
-
-        self.model.eval()
-        with torch.no_grad():
-            valLoss = 0
-            totalSamples = 0
-            correctSamples = 0
-
-            for i, (inputTensor, labelTensor) in enumerate(validationLoader):
-                inputTensor = inputTensor.to(self.device)
-                labelTensor = labelTensor.to(self.device)
-
-                validationOutputs = self.model(inputTensor)
-                valLoss += criterion(validationOutputs, labelTensor).item()
-                _, predicted = torch.max(validationOutputs.data, 1)
-                _, accLabels = torch.max(labelTensor.data, 1)
-                totalSamples += labelTensor.size(0)
-                correctSamples += (predicted == accLabels).sum().item()
-
-            valLoss /= len(validationLoader)
-            valAccuracy = correctSamples / totalSamples
-
-        return (valLoss, valAccuracy)
-    
-    def train(self, epochs: int, inputs: np.array, labels: np.array, optimzier: optim.Optimizer, criterion: nn.Module, batch_size: int=32, validation_data: tuple=None) -> list:
-        trainLoader = DataLoader(self._loadData(inputs,labels),batch_size=batch_size)
-
-        history = []
-        for e in range(epochs):
-            epochLoss = 0
-            for i, (inputTensor, labelTensor) in enumerate(trainLoader):
-                inputTensor = inputTensor.to(self.device)
-                labelTensor = labelTensor.to(self.device)
-
-                optimzier.zero_grad()
-                outputTensor =  self.model(inputTensor)
-                loss = criterion(outputTensor,labelTensor)
-                loss.backward()
-                optimzier.step()
-
-                epochLoss += loss.item()
-
-            valLoss, valAccuracy = None, None
-            if validation_data is not None:
-                valLoss, valAccuracy = self.validate(validation_data, batch_size, criterion)
-                
-            epochLoss /= len(trainLoader)
-            history.append((epochLoss, valLoss, valAccuracy))
-            print(f"Epoch: {e+1}; loss: {epochLoss:.4f}; valLoss: {valLoss:.4f}; valAcc: {valAccuracy:.4f}")
-
-        return history
